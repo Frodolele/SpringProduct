22c04382a33997561fe365b4c5766099778704ba
Frodolele <maks.tichomirov@yandex.ru>
2020-11-11 10:47:36 +0300

    add model

--- src/main/java/com/frodolele/springproduct/model/Product.java
+++ src/main/java/com/frodolele/springproduct/model/Product.java
@@ -1,5 +1,51 @@
 package com.frodolele.springproduct.model;
 
+// POJO class, which description product from DB
 public class Product {
-    
+
+    private Integer id;
+    private String name;
+    private String brand;
+    private Integer price;
+    private Integer quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
---
+++ src/main/java/com/frodolele/springproduct/services/ProductService.java
@@ -0,0 +1,21 @@
+package com.frodolele.springproduct.services;
+
+import com.frodolele.springproduct.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ProductService {
+
+    void create(Product product);
+
+    List<Product> findByNameOrBrand(String nameOrBrand);
+
+    boolean update(Product product, int id);
+
+    boolean delete(Product product, int id);
+
+    List<Product> getAllLeftovers();
+
+}
